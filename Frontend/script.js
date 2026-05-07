32cfac6ef288575780eea5089e2dbf0f6b5b1836
Emiko <emikotsemaye@gmail.com>
2026-05-07 03:17:34 -0400

    javascript added

---
+++ Frontend/script.js
@@ -0,0 +1,13 @@
+const exerciseList = document.getElementById("exrecisList")
+const addExerciseButton = document.getElementById("addExerciseButton");
+const saveButton = document.getElementById("saveButton");
+const finsihButton = document.getElementById("finishButton");
+
+const exerciseCount = document.getElementById("exerciseCount");
+const setsCount = document.getElementById("setsCount");
+
+
+const workoutTitle = document.getElementById("workoutTitle");
+const workoutDuration = document.getElementById("workoutDuration");
+
+
