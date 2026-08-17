2965a7509bfb67cf022d71285eb3d82ab264d6b7
Emiko-Codes <emikotsemaye@gmail.com>
2026-08-16 22:24:23 -0400

    Improve mobile layout and update app files

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,9 @@ dotenv.config(); // Loads variables from your .env file into process.env.
 const app = express(); // Creates the Express application.
 const PORT = process.env.PORT || 5001; // Use the environment port, or 5001 for local development.
 
-app.use(cors()); // Allows the frontend to make requests to this backend.
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));// Allows the frontend to make requests to this backend.
 app.use(express.json()); // Converts incoming JSON request bodies into req.body.
 
 app.use("/api", authRoutes); // Auth routes become /api/auth/signup, /api/auth/login, and /api/profile/info.
